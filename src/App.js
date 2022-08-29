72cf36a96a49cc2c5d95b344e4220a678da1fb75
green68 <eric.vercheval@hotmail.fr>
2022-08-29 23:50:42 +0200

    navlink implementation for all

--- src/App.js
+++ src/App.js
@@ -1,39 +1,83 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Content from './components/Content';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 import { getYear } from "./datas/datas";
-import { NavLink } from 'react-router-dom';
-import { FaHome } from 'react-icons/fa';
+import { NavLink } from "react-router-dom";
+import { FaBalanceScale, FaCalendar, FaHome, FaPiggyBank } from "react-icons/fa";
+import { BsBank } from "react-icons/bs";
 
-
-const dataYear =  getYear()
+const dataYear = getYear();
 
 export default function App() {
-  const [page, setPage] = useState('home');
-  const [year, setYear] = useState(dataYear)
+  const [page, setPage] = useState("home");
+  const [year, setYear] = useState(dataYear);
 
   // useEffect(() => {
   //   setYear(getYear())
   // }, [])
 
-  const menuHandler = menu => {
-    setPage(menu)
+  const menuHandler = (menu) => {
+    setPage(menu);
     console.log(`menu : ${menu}`);
-  }
+  };
 
   return (
     <div className="main">
-      <Header page={page} menuClick={menu => menuHandler(menu)} >
+      <Header page={page} menuClick={(menu) => menuHandler(menu)}>
         <NavLink 
-        to={"/"} >
-          <FaHome/>
+          to={'/'} 
+          className="btn btn-dark btn-menu"
+          // data-menu={Menu.Home}
+          // onClick={(e) => changePage(e)}
+          >
+          <FaHome />
           Accueil
+        </NavLink>
+        <NavLink
+          to={'/balance'}
+          className="btn btn-dark btn-menu"
+          // data-menu={Menu.Balance}
+          // onClick={(e) => changePage(e)}
+        >
+          <FaBalanceScale />
+          Balance
         </NavLink>
       </Header>
       <Content />
-      <Footer page={page} menuClick={menu => menuHandler(menu)}/>
+      <Footer page={page} menuClick={(menu) => menuHandler(menu)} >
+      <NavLink
+          to={'/year'}
+          className="btn btn-dark btn-menu"
+          // data-menu={Menu.Year}
+          // onClick={(e) => changePage(e)}
+        >
+          <FaCalendar />
+          NC
+          {/* {year ? year.id : "NC"} */}
+        </NavLink>
+        <NavLink
+          to={"/bank"}
+          className="btn btn-dark btn-menu"
+          // data-menu={Menu.Bank}
+          // onClick={(e) => changePage(e)}
+        >
+          <BsBank />
+          NC
+        </NavLink>
+
+        <NavLink
+          to={"/cash"}
+          className="btn btn-dark btn-menu"
+          // data-menu={Menu.Cash}
+          // onClick={(e) => changePage(e)}
+        >
+          <FaPiggyBank />
+          NC
+        </NavLink>
+      </Footer>
     </div>
   );
 }
