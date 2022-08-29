80ff297f889b05e08f5ce4b37c66247c5f0f99ae
green68 <eric.vercheval@hotmail.fr>
2022-08-29 17:44:51 +0200

    first implementation

---
+++ src/datas/datas.json.js
@@ -0,0 +1,78 @@
+
+const jsonDatas = `{
+  "name": "meistermann",
+  "years": [
+    {
+      "id": "2022",
+      "bank_report": "3000.00",
+      "cash_report": "236.28",
+      "bank_items": [
+        {
+          "id": "01",
+          "date": null,
+          "label": "achat billet de train stage Lyon",
+          "type": "CB",
+          "cheque": "",
+          "folio": "",
+          "mvt": "-285.50",
+          "checked": false
+        },
+        {
+          "id": "02",
+          "date": null,
+          "label": "retrait gab",
+          "type": "",
+          "cheque": "",
+          "folio": "",
+          "mvt": "-120.00",
+          "checked": false
+        },
+        {
+          "id": "03",
+          "date": null,
+          "label": "cotisation laurent",
+          "type": "",
+          "cheque": "",
+          "folio": "",
+          "mvt": "150.00",
+          "checked": false
+        }
+      ],
+      "cash_items": [
+        {
+          "id": "01",
+          "date": null,
+          "label": "restitution avance caisse philippe",
+          "type": "",
+          "credit": "40.00"
+        },
+        {
+          "id": "02",
+          "date": null,
+          "label": "avance caisse philippe",
+          "mvt": "-40.00"
+        },
+        {
+          "id": "2019-03",
+          "date": null,
+          "label": "achat tee-shirt alma",
+          "mvt": "10.00"
+        },
+        {
+          "id": "2019-04",
+          "date": null,
+          "label": "retrait banque",
+          "credit": "120.00"
+        },
+        {
+          "id": "2019-05",
+          "date": null,
+          "label": "paiement stage",
+          "mvt": "-10.00"
+        }
+      ]
+    }
+  ]
+}`
+
+export { jsonDatas }
