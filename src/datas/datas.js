0d22ae9827846121d66ba671df1a91f356bcfe66
green68 <eric.vercheval@hotmail.fr>
2022-09-05 04:04:14 +0200

    class implemention with methods

--- src/datas/datas.js
+++ src/datas/datas.js
@@ -1,19 +1,42 @@
-
-// const jsonData = require('./datas.json')
 import { User } from "../classes/user";
 import { jsonDatas } from "./datas.json.js"
 
-// console.log(JSON.parse(jsonDatas))
-const user = new User(jsonDatas);
-
-const getYear = () => {
-    
-    console.log(user);
-    console.log(user.years.getLast());
-    return user.years.getLast()
+class Datas {
+    constructor() {
+        this.user = new User({})
+    }
+    setUserFromJSON(datas = jsonDatas) {
+        this.user = new User(datas)
+    }
+    getUser() {
+        return this.user
+    }
+    saveToLocale() {
+        const userString = JSON.stringify(this.user)
+        
+        localStorage.setItem('user', userString)
+    }
+    saveToFile(filename) {
+        console.log("dans saveToFile");
+        function download(content, fileName, contentType) {
+            var a = document.createElement("a");
+            var file = new Blob([content], {type: contentType});
+            a.href = URL.createObjectURL(file);
+            a.download = fileName;
+            a.click();
+        }
+        
+        const datas = JSON.stringify(this.user, null, 3)
+        // console.log(datas);
+        download(datas, filename, 'text/plain');
+    }
+    isLocale() {
+        return localStorage.getItem('user') ? true : false
+    }
 }
 
+const datas = new Datas()
+
 export {
-    // init,
-    getYear,
+    datas
 }
