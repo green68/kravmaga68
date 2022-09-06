2f58f96af3f6473a203201d780c9c11297014c4a
green68 <eric.vercheval@hotmail.fr>
2022-09-06 03:42:19 +0200

    refactoring

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -8,20 +8,6 @@ const Accueil = () => {
         <img width="200px" alt="icon" src={icon} />
       </div>
       <h1>Comptabilité</h1>
-      {/* <div className="Hello">
-        <a
-          href="https://github.com/green68/kravmaga68"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Lien github
-          </button>
-        </a>
-      </div> */}
     </div>
   );
 };
