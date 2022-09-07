884a436e30e1fac28e0ee310406b3b8b3b706349
green68 <eric.vercheval@hotmail.fr>
2022-09-07 15:56:11 +0200

    design page, add buttons

--- src/pages/ToolsPage.jsx
+++ src/pages/ToolsPage.jsx
@@ -1,6 +1,28 @@
+import { Button } from "react-bootstrap"
+import { BsDownload } from "react-icons/bs"
+import { FaExclamation, FaFileDownload, FaFileUpload } from "react-icons/fa"
+
 const ToolsPage = (props) => {
     return (
-      <div>ToolsPage</div>
+      <div className="tools p-3">
+        <h1 className="text-center p-2">Outils</h1>
+        <div className="d-flex flex-column p-2 gap-4">
+          <Button size="lg" variant="outline-danger" className="btn-menu">
+            <FaExclamation/>
+            Effacer les données
+          </Button>
+          <Button size="lg" variant="outline-danger" className="btn-menu">
+            <FaFileUpload/>
+            Charger les données
+          </Button>
+          <Button size="lg" variant="outline-success" className="btn-menu">
+            <FaFileDownload/>
+            Sauvegarder les données
+          </Button>
+
+        </div>
+
+      </div>
     )
   }
   
