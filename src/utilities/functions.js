c59dbec7c7aca67a64944e1d9871e7d01be9b01e
green68 <eric.vercheval@hotmail.fr>
2022-09-09 00:16:02 +0200

    function objectToArray

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -13,14 +13,23 @@ const pathTo = menu => Pathname + menu
 
 const downloadFile = (content, fileName, contentType) => {
     var a = document.createElement("a");
-    var file = new Blob([content], {type: contentType});
+    var file = new Blob([content], { type: contentType });
     a.href = URL.createObjectURL(file);
     a.download = fileName;
     a.click();
 }
 
+const objectToArray = (obj) => {
+    const datas = []
+    for (const [key, value] of Object.entries(obj)) {
+        datas.push([key, value])
+    }
+    return datas
+}
+
 export {
     downloadFile,
     isJSON,
+    objectToArray,
     pathTo
 }
