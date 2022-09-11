ee38420e5f1e7e26d4078b7c5f90b333af142942
green68 <eric.vercheval@hotmail.fr>
2022-09-11 18:33:42 +0200

    first implementation

---
+++ src/components/UploadDatas.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { FaFileUpload } from "react-icons/fa"
+import Validation from "./Validation";
+
+function UploadDatas({ onClose, onUpload }) {
+    const [isShow, setisShow] = useState(true)
+
+    const loadFromFile = () => {
+        console.log("loadDatas");
+        setisShow(false)
+        debugger
+        // navigate(pathTo(Menu.Home));
+    }
+
+    const handleClose = (e) => {
+        console.log("handleClose dans UploadDatas");
+        setisShow(false)
+        onClose()
+    }
+
+    const datas = {
+        name: "uploadDatas",
+        title: "Charger des données",
+        message: "Cette action remplacera les données actuelles.",
+        color: "danger",
+        icon: <FaFileUpload />,
+        callback: loadFromFile
+    }
+
+    return (
+        <Validation show={isShow} onClose={handleClose} datas={datas} />
+    )
+}
+
+export default UploadDatas
