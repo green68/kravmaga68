b7ce3b2980ebb51c217bf02615d261457ce54a4e
green68 <eric.vercheval@hotmail.fr>
2022-09-12 16:08:03 +0200

    passing and changing datas

--- src/components/UploadDatas.jsx
+++ src/components/UploadDatas.jsx
@@ -1,31 +1,39 @@
 import { useState } from "react"
 import { FaFileUpload } from "react-icons/fa"
 import Validation from "./Validation";
+import DropFile from "./DropFile";
+import { Container } from "react-bootstrap";
+import { User } from "../classes/user";
 
 function UploadDatas({ onClose, onUpload }) {
     const [isShow, setisShow] = useState(true)
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+    const [userDatas, setUserDatas] = useState(null)
 
-    const loadFromFile = () => {
-        console.log("loadDatas");
-        setisShow(false)
-        debugger
-        // navigate(pathTo(Menu.Home));
+    const loadFromFile = (e) => {
+        console.log(userDatas);
+        onUpload(userDatas)
+        // TODO: send message for datas change
+        handleClose()
     }
 
     const handleClose = (e) => {
         console.log("handleClose dans UploadDatas");
+        console.log(userDatas);
         setisShow(false)
         onClose()
     }
 
-    // const datas = {
-    //     name: "uploadDatas",
-    //     // title: "Charger des données",
-    //     // message: "Cette action remplacera les données actuelles.",
-    //     // color: "danger",
-    //     // icon: <FaFileUpload />,
-    //     // callback: loadFromFile
-    // }
+    const handleDatas = (datas) => {
+        const obj = new User(datas)
+        if(obj) {
+            setIsButtonDisabled(false)
+            setUserDatas(datas)
+        } else {
+            setIsButtonDisabled(true)
+            setUserDatas(null)
+        }
+    }
 
     return (
         <Validation 
@@ -34,10 +42,14 @@ function UploadDatas({ onClose, onUpload }) {
             icon={<FaFileUpload/>}
             title={"Charger des données"}
             color={"danger"}
-            children={"Cette action remplacera les données actuelles."}
             callback={loadFromFile}
-            // datas={datas} 
-        />
+            isButtonDisabled={isButtonDisabled}
+        >
+            <Container className="d-flex flex-column align-items-center" >
+                <p>Cette action remplacera les données actuelles.</p>
+                <DropFile handleDatas={handleDatas} />
+            </Container>
+        </Validation>
     )
 }
 
