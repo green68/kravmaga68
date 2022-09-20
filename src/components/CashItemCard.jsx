14e08a607587f9d9aa1f4d1ed3753a11b563dbcc
green68 <eric.vercheval@hotmail.fr>
2022-09-20 15:55:34 +0200

    instance border-color & change design

--- src/components/CashItemCard.jsx
+++ src/components/CashItemCard.jsx
@@ -8,17 +8,20 @@ import { CashItem } from "../classes/CashItem";
  */
 const CashItemCard = ({datas}) => {
 
-    console.log(datas);
+    const color = Math.sign( parseFloat( datas.mvt)) === -1 ? "danger" : "success"
 
     return (
-        <Card className="bg-dark text-lght border border-secondary mb-3 p-3">
+        <Card className={`bg-dark text-lght border border-${color} mb-3 p-3`} >
             <Card.Title className="d-flex justify-content-between">
-                <span>{datas.date.toLocaleDateString()}</span>    
-                <span>Montant : {datas.mvt}</span>    
+                <span>{datas.date?.toLocaleDateString()}</span>    
+                <span>{datas.getMvt()}</span>    
 
             </Card.Title>
             <Card.Body>
-                Label : {datas.label}
+                <p className="text-truncate">
+                {datas.label}
+
+                </p>
             </Card.Body>
             <Card.Footer className="d-flex justify-content-between">
                 <span>{datas.type}</span>
