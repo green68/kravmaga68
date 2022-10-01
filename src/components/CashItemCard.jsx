7a94e6b70c953dc7fa0fca430c70df464d48d14d
green68 <eric.vercheval@hotmail.fr>
2022-10-01 07:18:30 +0200

    @ts-check & design with flex

--- src/components/CashItemCard.jsx
+++ src/components/CashItemCard.jsx
@@ -1,3 +1,5 @@
+// @ts-check
+import React from "react";
 import { CashItem } from "../classes/CashItem"
 // TODO: put in App.css ?
 import "./CashItemCard.css"
@@ -10,12 +12,12 @@ const CashItemCard = ({ datas }) => {
     const color = Math.sign(parseFloat(datas.mvt)) === -1 ? "danger" : "success"
 
     return (
-        <section className="item-card" style={{ '--border-color': `var(--bs-${color})` }}>
+        <section className={`item-card item-card-${color}`} >
             <div className="item-card__title">
-                <span>{datas.date?.toLocaleDateString()}</span>
-                <span className="text-uppercase">{datas.type}</span>
-                <span className="text-uppercase">{datas.folio}</span>
-                <span>{datas.getMvt()} €</span>
+                <div >{ datas.getDate().toLocaleDateString() }</div>
+                <div className="text-uppercase" >{datas.getFolio()}</div>
+                <div className="text-uppercase">{datas.type}</div>
+                <div style={{marginLeft: "auto"}} >{datas.getMvtEuro()}</div>
             </div>
             <div className="item-card__body">
                 <span className="text-truncate">
